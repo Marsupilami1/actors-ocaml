aa7a6ae181e78640dce80a9fa4df3750d2103dfe
Marsupilami1 <martin.andrieux59@gmail.com>
2022-06-02 13:21:07 +0200

    simpler process management

--- lib/actor.ml
+++ lib/actor.ml
@@ -1,16 +1,7 @@
 (* Module Actor *)
 open Effect.Deep
 
-type 'a process =
-  | MailReader
-  | Fill of 'a Promise.t * (unit -> 'a)
-  | Computation of (unit -> unit)
-
-(* For debugging purpose *)
-(* let print_process = function *)
-(*   | MailReader -> print_endline "MailReader" *)
-(*   | Fill _ -> print_endline "Fill" *)
-(*   | Computation _ -> print_endline "Computation" *)
+type 'a process = (unit -> unit)
 
 (* Type of actor, 's is the type of messages,
    'a is the return type of methods *)
@@ -23,28 +14,16 @@ type 'a process =
 (* But the actor would be parameterized on 's, of kind (* -> *), *)
 (* I Don't know if it's possible in OCaml *)
 type ('m, 's, 'a) t = {
-  mail_box : ('s * 'a Promise.t) Queue.t;
-  mail_mutex : Mutex.t;
   processes : 'a process Queue.t;
-  (* TODO: the memory should be attached to the Domain *)
   memory : 'm Domain.DLS.key;
   methods : ('m, 's, 'a) t -> 's -> 'a
 }
 
 let create init methods = {
-  mail_box = Queue.create ();
-  mail_mutex = Mutex.create ();
   processes = Queue.create ();
   memory = Domain.DLS.new_key init;
   methods = methods
 }
-
-let send self message =
-  Mutex.lock self.mail_mutex;
-  let p = Promise.create () in
-  Queue.push (message, p) self.mail_box;
-  Mutex.unlock self.mail_mutex;
-  p
 
 let get_memory self = Domain.DLS.get self.memory
 let set_memory self memory = Domain.DLS.set self.memory memory
@@ -55,32 +34,18 @@ let push_process self process =
 let get_process self =
   Queue.take_opt self.processes
 
-let read_mails self =
-  (* Do not spam the mail box *)
-  Mutex.lock self.mail_mutex;
-  if Queue.is_empty self.processes then Domain.cpu_relax ();
-  Queue.iter (fun (m, p) ->
-      push_process self @@ Fill(p, fun () -> self.methods self m)
-    ) self.mail_box;
-  (* Mutex on mailbox ? *)
-  Queue.clear self.mail_box;
-  Mutex.unlock self.mail_mutex
+let send self message =
+  let p = Promise.create () in
+  push_process self (fun () ->
+      Promise.fill p (self.methods self message));
+  p
 
 let manage_next_process self =
-  (* Never empty because of the mail_reader *)
-  let process = Option.get @@ get_process self in
-  begin
-    match process with
-    | MailReader ->
-      read_mails self;
-      push_process self MailReader
-    | Fill (p, f) ->
-      let v = f () in Promise.fill p v
-    | Computation f -> f ()
-  end
+  match get_process self with
+  | None -> ()
+  | Some f -> f ()
 
 let run self = ignore @@ Domain.spawn (fun _ ->
-    push_process self MailReader;
     let rec loop () =
       match_with manage_next_process self {
         retc = (fun _ -> loop ());
@@ -90,12 +55,10 @@ let run self = ignore @@ Domain.spawn (fun _ ->
           | Promise.NotReady p -> Some (
               fun (k : (a, _) continuation) ->
                 (* The process is waiting for the promise to be filled *)
-                (* So we add a hook to this promise to pusn the process *)
+                (* So we add a hook to this promise to push the process *)
                 (* back to the queue *)
                 Promise.add_hook p (fun v ->
-                    push_process self
-                      (Computation (fun _ -> continue k v))
-                  );
+                    push_process self (fun _ -> continue k v));
                 loop ();
             )
           | _ -> None

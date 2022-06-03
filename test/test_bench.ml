e0404ca2be271e967be524bf43e4b8995c57d876
Marsupilami1 <martin.andrieux59@gmail.com>
2022-06-03 09:21:54 +0200

    add test_bench

---
+++ test/test_bench.ml
@@ -0,0 +1,31 @@
+open Actorsocaml
+
+
+module MyMessage = struct
+  type 'a t = Pass : unit -> unit t
+  type method_type = { m : 'a . 'a t -> 'a }
+end
+
+module MyActor = Actor.Make(MyMessage)
+
+
+let methods :
+  type a . 'm MyActor.t -> a MyMessage.t -> a =
+  fun _ -> function
+    | Pass () -> ()
+let actor_methods self = {
+  MyMessage.m = fun s -> methods self s
+}
+
+let init = Fun.id
+
+let actor = MyActor.create init actor_methods
+
+let _ =
+  print_endline "-----TEST BENCH-----";
+
+  let message = MyMessage.Pass () in
+  MyActor.run actor;
+  ignore @@ MyActor.send actor message;
+  Unix.sleep 5;
+  print_endline "-------------------"

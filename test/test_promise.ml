7210670f2eeed0e29e12e481605260f3c54847be
Martin Andrieux <martin.andrieux59@gmail.com>
2022-06-14 09:39:03 +0200

    use alcotest for Promise

--- test/test_promise.ml
+++ test/test_promise.ml
@@ -1,71 +1,81 @@
 (* Test the functions of the Promise Module *)
-
 open Actorsocaml
 open Promise.Infix
 
-let test1 () =
+let test_fmap () =
   let p = Promise.pure 8 in
   let k = Promise.fmap ((+) 34) p in
-  assert (42 = Promise.await k);
+  Alcotest.(check int) "same int" 42 (Promise.await k);
   print_endline "Test 1 : Passed"
 
-let test2 () =
+let test_bind () =
   let p = Promise.pure 17 in
   let k = p >>= (fun v -> Promise.pure (v + 25)) in
-  assert (42 = Promise.await k);
+  Alcotest.(check int) "same int" 42 (Promise.await k);
   print_endline "Test 2 : Passed"
 
-let test3 () =
+let test_applicative () =
   let p = Promise.pure 21 in
   let pf = Promise.pure (( * ) 2) in
   let k = pf <*> p in
-  assert (42 = Promise.await k);
+  Alcotest.(check int) "same int" 42 (Promise.await k);
   print_endline "Test 3 : Passed"
 
-let test4 () =
+let test_callback () =
   let (p, fill) = Promise.create () in
-  Promise.add_callback p (fun v -> assert (v == 42));
+  Promise.add_callback p (fun v ->
+      Alcotest.(check int) "same int" 42 v);
   fill 42;
   print_endline "Test 4 : Passed"
 
-let test5 () =
+let test_join () =
   let (p, fillp) = Promise.create () in
   let (q, fillq) = Promise.create () in
   let k = Promise.join p in
   fillp q;
   fillq 42;
-  assert (42 = Promise.await k);
+  Alcotest.(check int) "same int" 42 (Promise.await k);
   print_endline "Test 5 : Passed"
 
-let test6 () =
+let test_unify () =
   let (p, fillp) = Promise.create () in
   let (q, _) = Promise.create () in
-  Promise.add_callback q (fun v -> assert (v = 42));
+  Promise.add_callback q (fun v ->
+      Alcotest.(check int) "same int" 42 v);
   Promise.unify q p;
   fillp 42;
-  assert (Promise.await p = 42);
-  assert (Promise.await q = 42);
+  Alcotest.(check int) "same int" 42 (Promise.await p);
+  Alcotest.(check int) "same int" 42 (Promise.await q);
   print_endline "Test 6 : Passed"
 
-let test7 () =
+let test_let () =
   let p = begin
     let* x = Promise.pure 40
     and* y = Promise.pure 2 in
     Promise.pure @@ x + y
   end in
-  assert (Promise.await p = 42);
+  Alcotest.(check int) "same int" 42 (Promise.await p);
   print_endline "Test 7 : Passed"
 
-let test8 () =
+let test_async () =
   let p = Promise.async (fun () -> 42) in
-  assert (Promise.await p = 42);
+  Alcotest.(check int) "same int" 42 (Promise.await p);
   print_endline "Test 8 : Passed"
 
 
 let main _ =
-  print_endline "-----TEST PROMISE-----";
-  test1 (); test2 (); test3 (); test4 ();
-  test5 (); test6 (); test7 (); test8 ();
-  print_endline "----------------------"
+  let open Alcotest in
+  run "Promise" [
+    "", [
+      test_case "fmap" `Quick test_fmap;
+      test_case "bind"         `Quick test_bind;
+      test_case "applicative"  `Quick test_applicative;
+      test_case "callback"     `Quick test_callback;
+      test_case "join"         `Quick test_join;
+      test_case "unify"        `Quick test_unify;
+      test_case "let"          `Quick test_let;
+      test_case "async"        `Quick test_async
+    ]
+  ]
 
 let _ = Actor.Main.run main

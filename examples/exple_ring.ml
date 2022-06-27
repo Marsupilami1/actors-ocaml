c73668dadac2a4ead515087f88d7b0a6497bcd1b
Martin Andrieux <martin.andrieux59@gmail.com>
2022-06-27 13:37:42 +0200

    update ring exemple

--- examples/exple_ring.ml
+++ examples/exple_ring.ml
@@ -2,84 +2,33 @@
 (* https://stw.gitbooks.io/the-encore-programming-language/content/nutshell.html *)
 open Actorsocaml
 
+let rec ring_member id = object%actor
+  val next = None
+  val id = id
+  method create_ring id size leader =
+    if id <= size then begin
+      next <- Option.some @@ ring_member id;
+      (Option.get next)#.create_ring (id + 1) size leader
+    end else
+      next <- Option.some leader
 
-module rec MessageRing : sig
-  type 'a t =
-    | Send : int -> unit t
-    | Stop : RingMember.t -> unit t
-    | CreateRing : int * int * RingMember.t -> unit t
-  type method_type = { m : 'a . ('a Promise.t -> 'a) -> 'a t -> 'a }
+  method send hops =
+    if hops > 0 then
+      (Option.get next)#.send (hops-1)
+    else begin
+      Printf.printf "%d\n%!" id
+    end
 end
-= struct
-  type 'a t =
-    | Send : int -> unit t
-    | Stop : RingMember.t -> unit t
-    | CreateRing : int * int * RingMember.t -> unit t
-  type method_type = { m : 'a . ('a Promise.t -> 'a) -> 'a t -> 'a }
-end
-and RingMember : sig
-  type t
-  val create : (t -> MessageRing.method_type) -> t
-  val send : t -> 'a MessageRing.t -> 'a Promise.t
-end = Actor.DefaultActor(MessageRing)
-
-type memory = {next : RingMember.t Option.t}
-let rec actor_ring_methods =
-  let init = fun _ -> { next = None } in
-  let mem = Domain.DLS.new_key init in
-  let ring_methods
-    : type a . RingMember.t
-      -> (a Promise.t -> a)
-      -> a MessageRing.t
-      -> a
-    = fun _ forward -> function
-      | Send(n) ->
-        if n < 0 then () else begin
-          (* Printf.printf "%d\n%!" n; *)
-          let m = Domain.DLS.get mem in
-          match m.next with
-          | None -> print_endline "abort"
-          | Some next -> forward @@ RingMember.send next (Send(n-1))
-        end
-      | CreateRing(id, size, leader) ->
-        (* let m = Domain.DLS.get mem in *)
-        if id >= size then begin
-          Domain.DLS.set mem {next = Some leader}
-        end else begin
-          let next = RingMember.create actor_ring_methods in
-          (* m.next <- Some next; *)
-          Domain.DLS.set mem {next = Some next};
-          (* Domain.DLS.set mem m; *)
-          forward @@ RingMember.send next (CreateRing(id+1, size, leader))
-        end
-      | Stop(leader) ->
-        let m = Domain.DLS.get mem in
-        let next = Option.get m.next in
-        (* m.next <- None; *)
-        Domain.DLS.set mem {next = None};
-        if next != leader then begin
-          forward @@ RingMember.send next (Stop(leader))
-        end
-  in fun self ->
-    {MessageRing.m = fun forward -> ring_methods self forward}
 
 let main _ =
   print_endline "-----TEST RING------";
-  let _ =
-    let leader = RingMember.create actor_ring_methods in
-    let p = RingMember.send leader (CreateRing(1, 503, leader)) in
-    Promise.await p;
-    print_endline "Creation: Done";
+  let leader = ring_member 1 in
+  leader#.create_ring 2 503 leader;
+  print_endline "Creation: Done";
 
-    let p' = RingMember.send leader (Send(10_000)) in
-    Promise.await p';
-    print_endline "Cycle: Done";
+  leader#.send 10000;
+  print_endline "Cycle: Done";
 
-    (* let p'' = RingMember.send leader (Stop(leader)) in *)
-    (* Promise.await p''; *)
-    (* RingMember.stop leader; *)
-    print_endline "Stop: Done";
-  in
   print_endline "Test passed";
   print_endline "--------------------"
 

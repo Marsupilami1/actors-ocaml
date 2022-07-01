6aa08406ec0d214e9bb3ea4ea26ca4fec184f293
Martin Andrieux <martin.andrieux59@gmail.com>
2022-07-01 09:37:23 +0200

    add quick sort

--- test/test_object.ml
+++ test/test_object.ml
@@ -7,6 +7,34 @@ open Actorsocaml
 (*     self#field *)
 (* end *)
 
+let split x xs =
+  let worker (l, g) e =
+    if e < x then (e :: l, g) else (l, e :: g)
+  in List.fold_left worker ([], []) xs
+
+let rec qsort l =
+  match l with
+  | [] -> []
+  | x::xs ->
+    let l, g = split x xs in
+    (qsort l) @ (x :: qsort g)
+
+let rec mk_sorter () =
+  object%actor
+    method sort l =
+      match l with
+      | [] -> []
+      | _ when List.length l < 100000 -> qsort l
+      | x::xs ->
+        let l, g = split x xs in
+        let sortl = mk_sorter () in
+        let sortg = mk_sorter () in
+        let pl = sortl#!sort l in
+        let pg = sortg#!sort g in
+        (Promise.await pl) @ (x :: (Promise.await pg))
+  end
+
+
 
 let main () =
   let a = object%actor
@@ -20,7 +48,13 @@ let main () =
   end in
   a#.set 42;
   let p = b#!get in
-  Printf.printf "a.y = %d\n"  @@ (Promise.get p)
+  Printf.printf "a.y = %d\n"  @@ (Promise.get p);
+
+  let l = List.init 100_000 (fun n -> 47 * n mod 367) in
+  let sorter = mk_sorter () in
+  print_endline "start";
+  let _sorted_l = sorter#.sort l in
+  print_endline "ok: sorted"
 (* Printf.printf "foo = %d\n"  @@ foo#field *)
 
 let _ = Actor.Main.run main

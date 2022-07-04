2896ffec25ffab4590f579199a69ed0a786cab0a
Drup <drupyog@zoho.com>
2022-07-04 15:01:59 +0200

    Refactor ackermann benchmark to allow comparison.

--- bench/ackermann/bench_ackermann.ml
+++ bench/ackermann/bench_ackermann.ml
@@ -1,10 +1,14 @@
 let samples = ref []
 let add_samples s = samples := s @ !samples
 
+let m = 3
+let n = 4
+let r = 50
+
 module Await = struct
   open Actorsocaml
 
-  let ackermann = object%actor (self)
+  let mk_ackermann () = object%actor (self)
     method compute m n =
       match m, n with
       | (0, _) -> n + 1
@@ -19,9 +23,7 @@ module Await = struct
   end
 
   let main () =
-    let m = 3 in
-    let n = 4 in
-    let r = 50 in
+    let ackermann = mk_ackermann () in
     let f () = Promise.get (ackermann#!compute m n) in
     add_samples @@
     Benchmark.latency1 ~name: "Ackerman(3, 4), await" (Int64.of_int r) f ()
@@ -29,54 +31,48 @@ module Await = struct
   let () = Actor.Main.run main
 end
 
-(* module Eio = struct
- *   open Eio
- * 
- *   let rec ackermann sw m n =
- *     Fiber.fork_promise ~sw (fun _ ->
- *         match m, n with
- *         | (0, _) -> n + 1
- *         | (_, 0) -> Promise.await_exn @@ ackermann sw (m - 1) 1
- *         | (_, _) ->
- *           let vz = Promise.await_exn @@ ackermann sw m (n - 1) in
- *           let result = ackermann sw (m - 1) vz in
- *           Promise.await_exn result
- *       )
- * 
- *   let main _ =
- *     let m = 3 in
- *     let n = 4 in
- *     let r = 1000 in
- * 
- *     let f () = Switch.run (fun sw -> Promise.await (ackermann sw m n)) in
- *     add_samples @@ Benchmark.latency1 ~name: "Ackerman(3, 4), await" (Int64.of_int r) f ()
- * 
- *   let () = Eio_main.run main
- * end *)
+module Eio = struct
+  open Eio
 
-(* module Forward = struct
- *   open Actorsocaml
- * 
- *   let ackermann = object%actor (self)
- *     method compute m n =
- *       match m, n with
- *       | (0, _) -> n + 1
- *       | (_, 0) ->
- *         self#!!compute (m - 1) 1
- *       | _ ->
- *         let vz = self#.compute m (n - 1) in
- *         self#!!compute (m - 1) vz
- *   end
- * 
- *   let main _ =
- *     let m = 3 in
- *     let n = 4 in
- *     let r = 100000 in
- *     let f () = Promise.get (ackermann#!compute m n) in
- *     add_samples @@ Benchmark.latency1 ~name: "Ackerman(3, 4), forward" (Int64.of_int r) f ()
- * 
- *   let () = Actor.Main.run main
- * end *)
+  let rec ackermann sw m n =
+    Fiber.fork_promise ~sw (fun _ ->
+        match m, n with
+        | (0, _) -> n + 1
+        | (_, 0) -> Promise.await_exn @@ ackermann sw (m - 1) 1
+        | (_, _) ->
+          let vz = Promise.await_exn @@ ackermann sw m (n - 1) in
+          let result = ackermann sw (m - 1) vz in
+          Promise.await_exn result
+      )
+
+  let main _ =
+    let f () = Switch.run (fun sw -> Promise.await (ackermann sw m n)) in
+    add_samples @@ Benchmark.latency1 ~name: "Ackerman(3, 4), eio" (Int64.of_int r) f ()
+
+  let () = Eio_main.run main
+end
+
+module Forward = struct
+  open Actorsocaml
+
+  let mk_ackermann () = object%actor (self)
+    method compute m n =
+      match m, n with
+      | (0, _) -> n + 1
+      | (_, 0) ->
+        self#!!compute (m - 1) 1
+      | _ ->
+        let vz = self#.compute m (n - 1) in
+        self#!!compute (m - 1) vz
+  end
+
+  let main _ =
+    let ackermann = mk_ackermann () in
+    let f () = Promise.get (ackermann#!compute m n) in
+    add_samples @@ Benchmark.latency1 ~name: "Ackerman(3, 4), forward" (Int64.of_int r) f ()
+
+  let () = Actor.Main.run main
+end
 
 let () =
   Benchmark.tabulate !samples
